5bcb8d1aa2732342dd175c2e4a4e1fd80c494b80
max9159 <max9159@gmail.com>
2016-01-14 03:22:47 +0800

    delete test object

--- LunchOrder/app/src/main/java/com/yee/launch/lunchorder/ActivityCustomList.java
+++ LunchOrder/app/src/main/java/com/yee/launch/lunchorder/ActivityCustomList.java
@@ -21,16 +21,11 @@ public class ActivityCustomList extends AppCompatActivity {
     private ListView listView;
     private ListView listView2;
     private ListView listView3;
-    //    private MyAdapter myAdapter;
     private MyBaseAdapter myAdapter;
-    //private ImageLoader imageLoader;
-
-    //private ArrayList<CardItem> cardItems;
     private ArrayList<FoodItem> foodItems;
 
     private Button btnLaunch;
     private FoodItemControl fdctrl;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +42,6 @@ public class ActivityCustomList extends AppCompatActivity {
 
     private void init() {
         this.context = this;
-        //this.imageLoader = UniversalImageLoaderHelper.getInstance(context);
     }
 
     private void initUI() {
@@ -94,97 +88,5 @@ public class ActivityCustomList extends AppCompatActivity {
         });
     }
 
-    private void initListView(ListView lv) {
-//        cardItems = new ArrayList<>();
-//        CardItem cardItem = null;
-//        //add KS
-//        for (int i = 0; i < 10; i++) {
-//            cardItem = (new CardItem())
-//                    .setBackgroundID(R.mipmap.taiwan_changhua)
-//                    .setName(getString(R.string.Changhua));
-//            cardItems.add(cardItem);
-//
-//            cardItem = (new CardItem())
-//                    .setBackgroundID(R.mipmap.taiwan_chiayi)
-//                    .setName(getString(R.string.Chiayi));
-//            cardItems.add(cardItem);
-//
-//            cardItem = (new CardItem())
-//                    .setBackgroundID(R.mipmap.taiwan_kaohsiung)
-//                    .setName(getString(R.string.Kaohsiung));
-//            cardItems.add(cardItem);
-//
-//            cardItem = (new CardItem())
-//                    .setBackgroundID(R.mipmap.taiwan_miaoli)
-//                    .setName(getString(R.string.Miaoli));
-//            cardItems.add(cardItem);
-//
-//            cardItem = (new CardItem())
-//                    .setBackgroundID(R.mipmap.taiwan_taichung)
-//                    .setName(getString(R.string.Taichung));
-//            cardItems.add(cardItem);
-//
-//            cardItem = (new CardItem())
-//                    .setBackgroundID(R.mipmap.taiwan_tainan)
-//                    .setName(getString(R.string.Tainan));
-//            cardItems.add(cardItem);
-//        }
-
-//        myAdapter = new MyAdapter(context);
-
-        myAdapter = new MyBaseAdapter(context,fdctrl.FoodListFromJson);
-        lv.setAdapter(myAdapter);
-
-    }
-
-
-//    static class ViewHolder {
-//        public ImageView ivWallpaper;
-//        public TextView tvName;
-//
-//    }
-//
-//    public class MyAdapter extends BaseAdapter {
-//        private LayoutInflater layoutInflater;
-//
-//        public MyAdapter(Context context) {
-//            layoutInflater = LayoutInflater.from(context);
-//        }
-//
-//        @Override
-//        public int getCount() {
-//            return cardItems == null ? 0 : cardItems.size();
-//        }
-//
-//        @Override
-//        public Object getItem(int i) {
-//            return i;
-//        }
-//
-//        @Override
-//        public long getItemId(int i) {
-//            return i;
-//        }
-//
-//        @Override
-//        public View getView(int position, View convertView, ViewGroup viewGroup) {
-//            ViewHolder holder = null;
-//            if (convertView == null) {
-//                holder = new ViewHolder();
-//                convertView = layoutInflater.inflate(R.layout.custom_list_item, null);
-//                holder.ivWallpaper = (ImageView) convertView.findViewById(R.id.ivWallpaper);
-//                holder.tvName = (TextView) convertView.findViewById(R.id.tvName);
-//
-//                convertView.setTag(holder);
-//            } else {
-//                holder = (ViewHolder) convertView.getTag();
-//            }
-//
-//            holder.ivWallpaper.setImageDrawable(getResources().getDrawable(cardItems.get(position).getBackgroundID()));
-//            holder.tvName.setText(cardItems.get(position).getName());
-//
-//            return convertView;
-//        }
-//    }
 
 }
